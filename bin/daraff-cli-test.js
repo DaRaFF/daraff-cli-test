08ebdb4060b620cf6f46182b4c3e135bbfb4a7d6
Ralph Meier <ralphmeier79@gmail.com>
2018-04-12 21:58:10 +0200

    fix: eslint

--- bin/daraff-cli-test.js
+++ bin/daraff-cli-test.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-const shell = require("shelljs")
-shell.exec("echo shell.exec works")
+const shell = require('shelljs')
+shell.exec('echo shell.exec works')
 console.log('daraff-cli-test -> the default executable')
